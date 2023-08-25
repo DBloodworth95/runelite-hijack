a081953140deba2ac7ff47b3c17cfd7cb13872e8
DBloodworth95 <dan.bloodworth95@gmail.com>
2023-08-25 14:34:00 +0000

    Add ClassByte object

---
+++ src/main/java/com/runelitehijack/object/ClassByte.java
@@ -0,0 +1,11 @@
+package com.runelitehijack.object;
+
+public final class ClassByte {
+    public String name;
+    public byte[] bytes;
+
+    public ClassByte(String name, byte[] bytes) {
+        this.name = name;
+        this.bytes = bytes;
+    }
+}
